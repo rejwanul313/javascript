34d3e62e9490aaa8f68b19b4e65d618db417a90b
Md Rejwanul Hoque <bulbul.rej@gmail.com>
2023-02-08 11:39:20 +0600

    Last In First Out and represents a stack

---
+++ LIFO-stack/stack.js
@@ -0,0 +1,10 @@
+// push(v) -- adds an element v at the end of the array (at index length) -- returns the new length of the array
+// pop() -- removes the last element -- returns the element that was deleted
+
+const arr = [1,2,3,4]
+arr.push(5);
+console.log(arr); //[1,2,3,4,5]
+
+last_element = arr.pop()
+console.log(arr) //[1,2,3,4]
+console.log(last_element) //5
